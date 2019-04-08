3aa6cfd8484762026c1f552fffdc8c0ed27acd10
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-08 19:19:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,2 @@
 def turn_count(board)
-return " #{board[0]} | #{board[1]} | #{board[2]} "
-return "-----------"
-return " #{board[3]} | #{board[4]} | #{board[5]} "
-return "-----------"
-return " #{board[6]} | #{board[7]} | #{board[8]} "
 end
